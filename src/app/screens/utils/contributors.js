46ccac6bde5f4a359ddde74806ca9dd153a1e99b
Joshua <koffworld@gmail.com>
2024-06-21 13:46:14 +0000

    another change

--- src/app/screens/utils/contributors.js
+++ src/app/screens/utils/contributors.js
@@ -1,36 +1,23 @@
 const contributors = [
     {
         id: 1,
-        name: 'Joshua Koffie',
+        name: 'Gideon Asare Twum',
         role:'Team Lead',
-        photo: '/images/josh.png'
+        photo: '/images/deon.jpg'
     },
     {
         id: 2,
         name: 'Jennifer Harrison',
-        role:'frontend Developer ',
-        photo: '/images/jennifer.png'
-    
+        role:'Frontend Developer ',
+        photo: '/images/jenny.jpg'
+
     },
     {
         id: 3,
-        name: 'Daniel Uzoma Iluonaze',
-        role:'Backend Developer ',
-        photo: '/images/daniel.png'
-    },
-    {
-        id: 4,
-        name: 'Frank Mugwaneza',
-        role:' Research Lead',
-        photo: '/images/frank.png'
-        
-    },
-    {
-        id: 5,
         name: 'Eunice M. Kesseh',
         role:' UI/UX Designer',
-        photo: '/images/eun.png'
+        photo: '/images/eu.jpg'
     }
     ]
-    
+
     export default contributors
